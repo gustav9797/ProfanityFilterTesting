35d28a1ab7ed37f5f13c6ca5409a530c96c463d0
eldsflame <eldsflame@users.noreply.github.com>
2016-03-01 21:29:53 +0100

    Added files via upload

---
+++ index_till_filtret.php
@@ -0,0 +1,14 @@
+<?php
+	require_once __DIR__ . '/vendor/autoload.php';
+
+	use Snipe\BanBuilder\CensorWords;
+	$censor = new CensorWords;
+	$censor->setReplaceChar("*");
+	if(!empty($_POST["test"]))
+		$input = $_POST["test"];
+	else
+		$input="This is a fucking bitch a$$ sh1t.";
+	$string = $censor->censorString($input);
+	echo "Original: " . $string["orig"] . "<br><br><br>";
+	echo "Clean: " . $string["clean"] . "<br><br><br><br>";
+?>
