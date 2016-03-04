59e794a55f65a0db336602834127c8d0b2b839e6
eldsflame <eldsflame@users.noreply.github.com>
2016-03-04 11:06:33 +0100

    Added files via upload

--- index.php
+++ index.php
@@ -5,60 +5,42 @@
 .stil
 {
 		background-color:pink;
-		width:300px;
+		width:278px;
 		height:auto;
 		margin-top:50px;
 		margin-left:auto; 
 		margin-right:auto;
 }
-} </style>
+textarea 
+{
+    resize: none;
+	margin-left:5px;
+}
+p
+{
+	margin-left:110px;
+	margin-right:auto;
+}
+input
+{
+	margin-left:110px;
+	margin-right:auto;	
+}
+</style>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Profanity Filter</title>
 <script>
-var i = 0;
-function change()
-{
-	if(i==0)
-	{
-		i=1;
-		document.getElementById("php").style.display="block";
-		document.getElementById("java").style.display="none";
-		
-	}
-	else
-	{
-		i=0;
-		document.getElementById("php").style.display="none";
-		document.getElementById("java").style.display="block";
-	}
-}
-function visa()
-{
-	document.getElementById("fel").innerHTML="här ska det hända något som fixar detta: "+ document.getElementById('textruta').value;
-}
 </script>
 </head>
 
 <body>
-<div id="java" class="stil">
-	<p>Fulfilter med java</p>
-	<textarea rows="4" cols="35"id="textruta"></textarea><br/>
-    <button onclick="visa()">Fulfilter</button><br/>
-    <button onclick="change()">php</button>
-    <p id="fel"></p>
-</div>
-
 <div id="php" class="stil">
-	<p>Fulfilter med php</p>
-		<form id="form1" name="form1" method="post" action="gymnasiearbete/index.php">
-		<textarea rows="4" cols="35"name="HelaTexten"></textarea>
+	<p>Fulfilter</p>
+	<form id="form1" name="form1" method="post" action="gymnasiearbete/index.php">
+		<textarea rows="6" cols="35"name="HelaTexten" draggable="false" ></textarea>
 		<br/>
 		<input name="Filtrera" type="submit" />
 	</form>
-<button onclick="change()">Java</button>
 </div>
-<script>
-document.getElementById("php").style.display="none";
-</script>
 </body>
 </html>
