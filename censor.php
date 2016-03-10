59eaac0738013e373743f0da177c25e8cf6403a5
gustav9797 <gustav9797@hotmail.se>
2016-03-10 15:04:52 +0100

    Verkar fungera

---
+++ censor.php
@@ -0,0 +1,64 @@
+﻿<?php
+	
+	class Censor {
+		
+		private $censorChar = "*";
+		
+		private $badwords = array("kuk", "fitta", "bög");
+		
+		private $wordlist = array();
+		
+		public function initialize() {
+			$this->wordlist = file("ordlista.txt");
+		}
+		
+		public function run($text) {
+			$output = array();
+			
+			$words = explode(" ", $text);
+			foreach($words as $word) {
+				$outputWord = $word;
+				if(in_array($word, $this->badwords)) {
+					echo(" censored \"" . $word . "\"");
+					$outputWord = $this->censorString($word);
+				} else if(!in_array($word, $this->wordlist)) {
+					//TODO: översätt från leet translator till svenska
+					//TODO: om finns med i lista med fula ord, filtrera bort
+					//echo(" |testing " . $word . "| ");
+					foreach($this->badwords as $badword) {
+						//echo(" |testing " . $badword . " in " . $word . "| ");
+						if(mb_strpos($word, $badword) !== false) {
+							//echo(" censored \"" . $word . "\"");
+							$outputWord = $this->censorStringInString($badword, $word);
+
+						}
+					}
+				}
+				
+				array_push($output, $outputWord);
+			}
+			$outputString = "";
+			foreach($output as $outputWord) {
+				$outputString .= ($outputWord . " ");
+			}
+			return $outputString;
+			//TODO: remove empty space at end of output string
+		}
+		
+		private function censorString($string) {
+			$censoredWord = preg_replace('/.*/', '*', $string);
+			return $censoredWord;
+		}
+		
+		private function censorStringInString($string, $mother) {
+			$output = $mother;
+			$pos = mb_strpos($mother, $string);
+			if(is_numeric($pos)) {
+				for($i = $pos; $i < strlen($string) && $i < strlen($mother); ++$i)
+					$output{$i} = $this->censorChar;
+			} else 
+				echo("There was an issue with the word \"" . $mother . "\"");
+			return $output;
+		}
+	}
+?>
--- index.php
+++ index.php
@@ -1,73 +1,73 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<style type="text/css">
-.stil
-{
-		background-color:pink;
-		width:278px;
-		height:auto;
-		margin-top:50px;
-		margin-left:auto; 
-		margin-right:auto;
-}
-.text
-{
-		background-color:pink;
-		width:auto;
-		height:auto;
-		margin-top:50px;
-		margin-left:100px; 
-		margin-right:100px;
-}
-textarea 
-{
-    resize: none;
-	margin-left:5px;
-}
-p
-{
-	margin-left:110px;
-	margin-right:auto;
-}
-input
-{
-	margin-left:110px;
-	margin-right:auto;	
-}
-</style>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Profanity Filter</title>
-<script>
-</script>
-</head>
+﻿<html>
+	<head>
+		<style type="text/css">
+		.stil
+		{
+				background-color:pink;
+				width:278px;
+				height:auto;
+				margin-top:50px;
+				margin-left:auto; 
+				margin-right:auto;
+		}
+		.text
+		{
+				background-color:pink;
+				width:auto;
+				height:auto;
+				margin-top:50px;
+				margin-left:100px; 
+				margin-right:100px;
+		}
+		textarea 
+		{
+			resize: none;
+			margin-left:5px;
+		}
+		p
+		{
+			margin-left:110px;
+			margin-right:auto;
+		}
+		input
+		{
+			margin-left:110px;
+			margin-right:auto;	
+		}
+		</style>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>Profanity Filter</title>
+	</head>
 
 <body>
-<div id="php" class="stil">
-	<p>Fulfilter</p>
-	<form id="form1" name="form1" method="post" action="index.php" enctype="multipart/form-data">
-		<textarea rows="6" cols="35"name="HelaTexten" draggable="false" ></textarea>
-		<br/>
-		<input name="Filtrera" type="submit" />
-	</form>
-</div>
+	<div id="php" class="stil">
+		<p>Fulfilter</p>
+		<form id="form1" name="form1" method="post" action="index.php" enctype="multipart/form-data">
+			<textarea rows="6" cols="35"name="HelaTexten" draggable="false" ></textarea>
+			<br/>
+			<input name="Filtrera" type="submit" />
+		</form>
+	</div>
+	
+	<?php
+		//include("CensorWords.php");
+		include("censor.php");
+		//$censor = new CensorWords;
+		//$censor->setReplaceChar("*");
+		$censor = new Censor();
+		$censor->initialize();
+		if(!empty($_POST["HelaTexten"]))
+		{
+			echo "<div class='text'>";
+			$input = $_POST["HelaTexten"];
+			$string = $censor->run($input);
+			//$string = $censor->censorString($input);
+			//echo "Original: " . $string["orig"] . "<br><br><br>";
+			//echo "Clean: " . $string["clean"] . "<br><br><br><br>";
+			echo "Clean: " . $string . "<br><br><br><br>";
+			echo "</div>";
+			unset($_POST);
+		}
+	?>
 </body>
 </html>
-<?php
-	include("CensorWords.php");
-	$censor = new CensorWords;
-	$censor->setReplaceChar("*");
-	if(!empty($_POST["HelaTexten"]))
-	{
-		echo "<div class='text'>";
-		$input = $_POST["HelaTexten"];
-		$string = $censor->censorString($input);
-		echo "Original: " . $string["orig"] . "<br><br><br>";
-		echo "Clean: " . $string["clean"] . "<br><br><br><br>";
-		echo "</div>";
-		header( 'Cache-Control: no-store, no-cache, must-revalidate' ); 
-		header( 'Cache-Control: post-check=0, pre-check=0', false ); 
-		header( 'Pragma: no-cache' );
-		unset($_POST);
-	}
-?>
