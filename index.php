5634bd429c4b9f92eb191c710cbe490aac15a1c3
eldsflame <eldsflame@users.noreply.github.com>
2016-03-10 12:11:00 +0100

    index

--- index.php
+++ index.php
@@ -11,6 +11,15 @@
 		margin-left:auto; 
 		margin-right:auto;
 }
+.text
+{
+		background-color:pink;
+		width:auto;
+		height:auto;
+		margin-top:50px;
+		margin-left:100px; 
+		margin-right:100px;
+}
 textarea 
 {
     resize: none;
@@ -36,7 +45,7 @@ input
 <body>
 <div id="php" class="stil">
 	<p>Fulfilter</p>
-	<form id="form1" name="form1" method="post" action="gymnasiearbete/index.php">
+	<form id="form1" name="form1" method="post" action="index.php" enctype="multipart/form-data">
 		<textarea rows="6" cols="35"name="HelaTexten" draggable="false" ></textarea>
 		<br/>
 		<input name="Filtrera" type="submit" />
@@ -44,3 +53,23 @@ input
 </div>
 </body>
 </html>
+<?php
+	require_once __DIR__ . '/vendor/autoload.php';
+
+	use Snipe\BanBuilder\CensorWords;
+	$censor = new CensorWords;
+	$censor->setReplaceChar("*");
+	if(!empty($_POST["HelaTexten"]))
+	{
+		echo "<div class='text'>";
+		$input = $_POST["HelaTexten"];
+		$string = $censor->censorString($input);
+		echo "Original: " . $string["orig"] . "<br><br><br>";
+		echo "Clean: " . $string["clean"] . "<br><br><br><br>";
+		echo "</div>";
+		header( 'Cache-Control: no-store, no-cache, must-revalidate' ); 
+		header( 'Cache-Control: post-check=0, pre-check=0', false ); 
+		header( 'Pragma: no-cache' );
+		unset($_POST);
+	}
+?>
