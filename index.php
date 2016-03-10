5f3940223de73642e8fd58afa6cbaaf685030cf7
gustav9797 <gustav9797@hotmail.se>
2016-03-10 12:09:41 +0100

    fixade dumheter

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -45,51 +44,3 @@ input
 </div>
 </body>
 </html>
-=======
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<style type="text/css">
-.stil
-{
-		background-color:pink;
-		width:278px;
-		height:auto;
-		margin-top:50px;
-		margin-left:auto; 
-		margin-right:auto;
-}
-textarea 
-{
-    resize: none;
-	margin-left:5px;
-}
-p
-{
-	margin-left:110px;
-	margin-right:auto;
-}
-input
-{
-	margin-left:110px;
-	margin-right:auto;	
-}
-</style>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Profanity Filter</title>
-<script>
-</script>
-</head>
-
-<body>
-<div id="php" class="stil">
-	<p>Fulfilter</p>
-	<form id="form1" name="form1" method="post" action="gymnasiearbete/index.php">
-		<textarea rows="6" cols="35"name="HelaTexten" draggable="false" ></textarea>
-		<br/>
-		<input name="Filtrera" type="submit" />
-	</form>
-</div>
-</body>
-</html>
->>>>>>> origin/master
