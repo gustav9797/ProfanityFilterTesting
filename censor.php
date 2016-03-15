7e47e092e17ffcd9871c9eaf930e37e2a73d4fad
gustav9797 <gustav9797@hotmail.se>
2016-03-15 12:17:31 +0100

    Fix special characters ignored

--- censor.php
+++ censor.php
@@ -19,28 +19,30 @@
 			$words = explode(" ", $text);
 			foreach($words as $word) {
 				$outputWord = $word;
-				$lowerWord = strtolower($word);
+				$lowerWord = strtolower(preg_replace("/[^A-Za-z0-9åäöÅÄÖ ]/", '', $word));
 				if(in_array($lowerWord, $this->badwords)) {
 					//echo(" censored \"" . $word . "\"");
 					$outputWord = $this->censorString($word);
 				} else if(!in_array($lowerWord, $this->wordlist)) {
 					//TODO: översätt från leet translator till svenska
 					//TODO: om finns med i lista med fula ord, filtrera bort
-					//echo(" |testing " . $word . "| ");
+					//echo(" |testing " . $lowerWord . "| ");
 					$current = $lowerWord;
 					foreach($this->badwords as $badword) {
 						//echo(" |testing " . $badword . " in " . $word . "| ");
 						if(mb_strpos($lowerWord, $badword) !== false) {
-							echo(" censored \"" . $word . "\"");
+							//echo(" censored \"" . $word . "\"");
 							$current = $this->censorStringInString($badword, $current);
 
 						}
 					}
-					$outputWord = $current;
+					if(strcmp($current, $lowerWord) != 0)
+						$outputWord = $current;
 				}
 				
 				array_push($output, $outputWord);
 			}
+			//echo("<br>");
 			$outputString = "";
 			foreach($output as $outputWord) {
 				$outputString .= ($outputWord . " ");
