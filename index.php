b2062b4c4888cef6a741d2494413890fbf05ff7d
eldsflame <pontus.swahn@hotmail.com>
2016-03-17 14:57:49 +0100

    uppdaterat lite

--- index.php
+++ index.php
@@ -69,14 +69,13 @@
 			unset($_POST);
 			if(1==0)
 			{
-				var int;
 				$conn=new PDO("mysql:host=127.0.0.1;dbname=gymnasiearbete;charset=UTF8","root","");
 				$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-				$sql="INSERT INTO `inlagg`(`Text`, `Fulhet`) VALUES ($string,int);
-				$stmt=$conn->prepare($sql);
-				$stmt->execute();
-				
-			}
+				$sql="INSERT INTO 'inlagg'('Text', 'Fulhet') VALUES (\"$string\",0)";
+                echo $sql;
+				$conn->exec($sql);
+            }
+			
 		}
 	?>
 </body>
