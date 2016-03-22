c696b7c5513bf60c7b83f179e6a66d9154c8778b
gustav9797 <gustav9797@hotmail.se>
2016-03-22 13:09:17 +0100

    Det upptäcker konstiga leet-tecken nu

--- censor.php
+++ censor.php
@@ -18,6 +18,38 @@
 
 			$this->badwords = file("Svenska.txt", FILE_IGNORE_NEW_LINES);
 			$this->wordlist = file("ordlista.txt", FILE_IGNORE_NEW_LINES);
+			
+			$leet_replace = array();
+			$leet_replace['a'] = '(a|a\.|a\-|4|@|Á|á|À|Â|à|Â|â|Ã|ã|α|Δ|Λ|λ)';
+			$leet_replace['b'] = '(b|b\.|b\-|8|\|3|ß|Β|β)';
+			$leet_replace['c'] = '(c|c\.|c\-|Ç|ç|¢|€|<|\(|{|©)';
+			$leet_replace['d'] = '(d|d\.|d\-|&part;|\|\)|Þ|þ|Ð|ð)';
+			$leet_replace['e'] = '(e|e\.|e\-|3|€|È|è|É|é|Ê|ê|∑)';
+			$leet_replace['f'] = '(f|f\.|f\-|ƒ)';
+			$leet_replace['g'] = '(g|g\.|g\-|6|9)';
+			$leet_replace['h'] = '(h|h\.|h\-|Η)';
+			$leet_replace['i'] = '(i|i\.|i\-|!|\||\]\[|]|1|∫|Ì|Í|Î|Ï|ì|í|î|ï)';
+			$leet_replace['j'] = '(j|j\.|j\-)';
+			$leet_replace['k'] = '(k|k\.|k\-|Κ|κ)';
+			$leet_replace['l'] = '(l|1\.|l\-|!|\||\]\[|]|£|∫|Ì|Í|Î|Ï)';
+			$leet_replace['m'] = '(m|m\.|m\-)';
+			$leet_replace['n'] = '(n|n\.|n\-|η|Ν|Π)';
+			$leet_replace['o'] = '(o|o\.|o\-|0|Ο|ο|Φ|¤|°|ø)';
+			$leet_replace['p'] = '(p|p\.|p\-|ρ|Ρ|¶|þ)';
+			$leet_replace['q'] = '(q|q\.|q\-)';
+			$leet_replace['r'] = '(r|r\.|r\-|®)';
+			$leet_replace['s'] = '(s|s\.|s\-|5|\$|§)';
+			$leet_replace['t'] = '(t|t\.|t\-|Τ|τ)';
+			$leet_replace['u'] = '(u|u\.|u\-|υ|µ)';
+			$leet_replace['v'] = '(v|v\.|v\-|υ|ν)';
+			$leet_replace['w'] = '(w|w\.|w\-|ω|ψ|Ψ)';
+			$leet_replace['x'] = '(x|x\.|x\-|Χ|χ)';
+			$leet_replace['y'] = '(y|y\.|y\-|¥|γ|ÿ|ý|Ÿ|Ý)';
+			$leet_replace['z'] = '(z|z\.|z\-|Ζ)';
+			$leet_replace['å'] = '(å|å\.|å\-|Å)';
+			$leet_replace['ä'] = '(ä|ä\.|ä\-|Ä)';
+			$leet_replace['ö'] = '(ö|ö\.|ö\-|Ö)';
+			$this->leet_replace = $leet_replace;
 		}
 		
 		public function run($text) {
@@ -26,7 +58,15 @@
 			$words = explode(" ", $text);
 			foreach($words as $word) {
 				$outputWord = $word;
-				$lowerWord = strtolower(preg_replace("/[^A-Za-z0-9åäöÅÄÖ ]/", '', $word));
+				//$lowerWord = strtolower(preg_replace("/[^A-Za-z0-9åäöÅÄÖ ]/", '', $word));
+				$lowerWord = $word;
+				foreach($this->leet_replace as $key => $value) {
+					//echo "key " . $key . " value " . $value;
+					$lowerWord = preg_replace('/' . $value . '/i', $key, $lowerWord);
+
+				}
+				//$lowerWord = str_ireplace(array_values($this->leet_replace), array_keys($this->leet_replace), $word);
+				//echo "<br/>" . $lowerWord;
 				if(in_array($lowerWord, $this->badwords)) {
 					//echo(" censored \"" . $word . "\"");
 					$outputWord = $this->censorString($word);
